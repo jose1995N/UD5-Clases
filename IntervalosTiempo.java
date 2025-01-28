3f7a2d5ce5c646ebb0ac94706ab1ed664aca27f3
jose1995N <jmatmor313@g.educaand.es>
2025-01-28 09:38:48 +0100

    Add files via upload

--- IntervalosTiempo.java
+++ IntervalosTiempo.java
@@ -4,8 +4,11 @@ public class IntervalosTiempo {
     public static void main(String[] args) {
         Tiempo tiempo = new Tiempo(10, 20, 40);
         Tiempo tiempo2 = new Tiempo(11);
-        
+        Tiempo suma = tiempo.suma(tiempo2);
+        Tiempo resta = tiempo.resta(tiempo2);
         System.out.println("El tiempo es :" + tiempo);
         System.out.println("Tiempo 2 es " + tiempo2);
+        System.out.println("Suma: " + suma);
+        System.out.println("Resta: " + resta);
     }
 }
--- Tiempo.java
+++ Tiempo.java
@@ -21,9 +21,45 @@ public class Tiempo {
         this.minutos = m;
         this.segundos= 0;
     }
-
+// METODO TOSTING 
     @Override
     public String toString(){
         return (this.horas +"h "+ this.minutos + "m " + this.segundos + "s"); 
     }
+//metodos
+
+//metodo de la suma 
+public Tiempo suma (Tiempo t){
+  int nuevasHoras = this.horas + t.horas;
+  int nuevosMinutos = this.minutos + t.minutos;
+  int nuevosSegundos = this.segundos + t.segundos;
+  // Normalización
+  if (nuevosSegundos >= 60) {
+    nuevosMinutos += nuevosSegundos / 60;
+    nuevosSegundos %= 60;
+}
+if (nuevosMinutos >= 60) {
+    nuevasHoras += nuevosMinutos / 60;
+    nuevosMinutos %= 60;
+}
+
+return new Tiempo(nuevasHoras, nuevosMinutos, nuevosSegundos);
+}
+public Tiempo resta(Tiempo t) {
+    int nuevasHoras = this.horas - t.horas;
+    int nuevosMinutos = this.minutos - t.minutos;
+    int nuevosSegundos = this.segundos - t.segundos;
+
+    // Normalización
+    if (nuevosSegundos < 0) {
+        nuevosSegundos += 60;
+        nuevosMinutos -= 1;
+    }
+    if (nuevosMinutos < 0) {
+        nuevosMinutos += 60;
+        nuevasHoras -= 1;
+    }
+    
+    return new Tiempo(nuevasHoras, nuevosMinutos, nuevosSegundos);
+}
 }
